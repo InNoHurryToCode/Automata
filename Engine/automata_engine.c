999cbb3836cf1ba63b169377acf52d39500601e2
Merijn Hendriks <kahvana166@gmail.com>
2019-03-14 22:05:39 +0100

    Remove debug messgaes

--- Engine/automata_engine.c
+++ Engine/automata_engine.c
@@ -5,9 +5,6 @@
 
 int main() {
 	/* intialize application */
-	printf("Automata Engine\n");
-	printf("Version: %c%d.%d.%d\n", AUTOMATA_VERSION_TYPE, AUTOMATA_VERSION_MAJOR, AUTOMATA_VERSION_MINOR, AUTOMATA_VERSION_FIX);
-
 	automataWindowInit();
 	automataWindowCreate(1280, 720, "Automata Engine");
 
