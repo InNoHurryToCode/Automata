9c830b357b7844a79f4661c2b719007e7627d96e
Merijn Hendriks <kahvana166@gmail.com>
2019-03-14 18:55:52 +0100

    Initial code

---
+++ Engine/automata_engine.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "automata_engine.h"
+
+int main() {
+	/* intialize application */
+	printf("Automata Engine\n");
+	printf("Version: %c%d.%d.%d\n", AUTOMATA_VERSION_TYPE, AUTOMATA_VERSION_MAJOR, AUTOMATA_VERSION_MINOR, AUTOMATA_VERSION_FIX);
+
+	/* application loop */
+
+	/* terminate application */
+	return EXIT_SUCCESS;
+}
