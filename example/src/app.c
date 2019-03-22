206701e9bc8737efb03f73b6fc2c03c53655c6cb
Merijn Hendriks <innohurrytocode@gmail.com>
2019-03-22 03:50:18 +0100

    Add window icon to example

--- example/src/app.c
+++ example/src/app.c
@@ -1,11 +1,24 @@
 #include <stdlib.h>
-#include <automata\automata_input.h>
 #include <automata\automata_window.h>
+#include <automata\automata_input.h>
+#include <automata\automata_image.h>
 
 int main() {
+	/* variables */
+	AutomataImage *windowIcon = NULL;
+
 	/* intialize application */
 	automataWindowInit();
 	automataWindowCreate(1280, 720, "Automata Engine");
+
+	/* set window icon*/
+	windowIcon = automataImageLoad("sprites/icon.png");
+
+	if (windowIcon) {
+		automataWindowSetIcon(windowIcon->width, windowIcon->height, windowIcon->pixels);
+		automataImageUnload(windowIcon);
+	}
+	
 	automataInputInit(automataWindowGetWindow());
 
 	/* application loop */
